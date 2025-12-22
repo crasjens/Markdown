90e95d4217c9e95952f623f7bc9a84edb1c50522
crasjens <crasjens@gmail.com>
2025-12-22 13:54:21 +0100

    test scripit igen

--- docs/assets/scripts.js
+++ docs/assets/scripts.js
@@ -2,7 +2,24 @@
 import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
 
 // Denne linje fortæller Mermaid, at den skal lede efter diagrammer på siden
-mermaid.initialize({ 
-    startOnLoad: true,
-    theme: 'default' // Du kan også bruge 'dark', 'forest', etc.
+mermaid.initialize({
+	startOnLoad: true,
+	theme: 'default', // Du kan også bruge 'dark', 'forest', etc.
 });
+
+// 1. Konfigurer MathJax (valgfrit, men godt for kontrollen)
+window.MathJax = {
+	tex: {
+		inlineMath: [
+			['$', '$'],
+			['\\(', '\\)'],
+		],
+	},
+};
+
+// 2. Skab et nyt script-element "i luften"
+const script = document.createElement('script');
+script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+script.async = true;
+
+// 3. Sæt det ind på siden
